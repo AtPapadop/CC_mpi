f031d7cafb6a0208448dd1d5856378baad68454c
atpapadop <atpapadop@it.auth.gr>
2025-12-09 22:15:24 +0200

    slurm script

---
+++ run_benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=cc_mpi_benchmark
+#SBATCH --output=cc_mpi_benchmark.out
+#SBATCH --error=cc_mpi_benchmark.err
+#SBATCH --ntasks=120
+#SBATCH --time=01:00:00
+#SBATCH --partition=batch
+
+module load gcc/9.4.0-eewq4j6  openmpi/5.0.3-6ysrffb matio
+
+export GRAPH_NAME="com-Friendster"
+
+cd ~/CC_mpi/
+mpirun -np $SLURM_NTASKS bin/mpi_cc_benchmark -r 5 -c 1024,2048,4096,8192 -e 1,2,4,8 -f data/${GRAPH_NAME}.mat -o results/${GRAPH_NAME}/
