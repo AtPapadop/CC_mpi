4213757d589ecb23703eb79b625105732d0bb158
atpapadop <atpapadop@it.auth.gr>
2025-12-15 19:29:01 +0200

    neww slurm scripts

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -5,12 +5,12 @@
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1         # 1 MPI rank per node
 #SBATCH --cpus-per-task=128         # 128 cores for OpenMP
-#SBATCH --time=00:30:00
+#SBATCH --time=1:30:00
 #SBATCH --partition=rome
 
 module load gcc/13.2.0-iqpfkya  openmpi/5.0.3-rhzbeym matio
 
-export GRAPH_NAME="com-Friendster"
+export GRAPH_NAMES=("com-Friendster" "mycielskian20")
 
 # OpenMP setup
 export OMP_NUM_THREADS=128
@@ -19,4 +19,6 @@ export OMP_PROC_BIND=spread
 
 cd ~/CC_mpi/
 
-mpirun -np $SLURM_NTASKS ./bin/mpi_cc_benchmark -r 5 -c 1024,2048,4096,8192 -e 1,2,4,8 -o results/${GRAPH_NAME}/ data/${GRAPH_NAME}.mat
+for GRAPH_NAME in "${GRAPH_NAMES[@]}"; do
+    mpirun -np $SLURM_NTASKS ./bin/mpi_cc_benchmark -r 2 -c 8192 -e 1,2,4,8 -o results/${GRAPH_NAME}/ data/${GRAPH_NAME}.mat
+done
